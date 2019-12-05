0b30f9720e28cbf9297536a31e533650d3d74429
hdtopku <1450948930@qq.com>
2019-12-05 22:15:01 +0800

    edu: 优化验证步骤

--- src/components/apm.vue
+++ src/components/apm.vue
@@ -9,10 +9,8 @@
   </ul>
   <el-divider></el-divider>
   <div style="display:flex; float:right;">
-    <el-link href="http://music.taojingling.cn/ap" style="margin-right:20px; z-index:1;"
+    <el-link href="http://music.taojingling.cn/am" style="margin-right:20px; z-index:1;"
     type="primary" target="_blank">教程</el-link>
-    <el-link href="https://www.myunidays.com/CN/zh-CN" style="margin-right:20px; z-index:1;"
-    type="primary" target="_blank">UNIDAYS</el-link>
     <el-link href="https://mail.pku.edu.cn/" type="primary" style="z-index:1;" target="_blank">打开邮箱</el-link>
   </div>
   <el-tabs v-model="activeName">
@@ -67,22 +65,28 @@ export default {
       activeName: 'first',
 
       fast: [
-        ['开始', '复制长链接到浏览器进行验证，具体参考：http://music.taojingling.cn/ap'],
         ['用多久？', '1到4年，苹果官方会抽取部分用户再次验证。', '如果被抽中了，需要换个邮箱重新验证；', '如果没被抽中，就可以接着用哈。'],
         ['查看订阅？', '1、打开 设置', '2、点击 Apple ID、iCloud、iTunes 与 Apple Store', '3、点击 订阅'],
+        ['补充验证？', '补充验证（苹果丢件）：', '1、第一项，填刚刚的邮箱', '2、第二项，填pku，并选择列表第一项PKU', '3、点击 Continue 继续', '完成后，截图给我'],
         ['长链接无效？', '情况一（体验或订阅过 Apple Music）：', '1、打开 设置', '2、点击 Apple ID、iCloud、iTunes 与 Apple Store', '3、点击 订阅', '4、点击 Apple Music 会员资格', '5、选择 学生（1个月）',
           '情况二（从未用过 Apple Music）：', '1、打开 Apple Music', '2、点击 为你推荐', '3、点击 立即体验', '4、选择 大学生', '5、点击 开始试用', '6、点击 验证资格',
           '跳转到Safari后，截图给我。'
         ]
       ],
       fastText: [
-        `复制长链接到浏览器进行验证，具体参考：http://music.taojingling.cn/ap`,
-        `1到4年，苹果官方会抽取部分用户再次验证。如果被抽中，需重新下单验证；
-否则，可接着用哈。`,
+        `1到4年，苹果官方会抽取部分用户再次验证。
+如果被抽中了，需要换个邮箱重新验证；
+如果没被抽中，就可以接着用哈。`,
         `查看订阅：
 1、打开 设置
 2、点击 Apple ID、iCloud、iTunes 与 Apple Store
 3、点击 订阅`,
+        `补充验证（苹果丢件）：
+1、第一项，填刚刚的邮箱
+2、第二项，填pku，并选择列表第一项PKU
+3、点击 Continue 继续
+
+完成后，截图给我`,
         `情况一（体验或订阅过 Apple Music）：
 1、打开 设置
 2、点击 Apple ID、iCloud、iTunes 与 Apple Store
@@ -103,22 +107,16 @@ export default {
       ],
 
       standby: [
-        ['好评？', ':) 之后遇到任何问题，随时联系哈。满意的话给小店一个好评哦~'],
-        ['补充验证？', '补充验证（苹果丢件）：', '1、第一项，填刚刚的邮箱', '2、第二项，填pku，并选择列表第一项PKU', '3、点击 Continue 继续', '完成后，截图给我'],
+        [':) 之后遇到任何问题，随时联系哈。满意的话给小店一个好评哦~'],
         ['二、补发？', '补充步骤（苹果邮件延迟或丢失）：', '1、点击 Verify on campus', '2、点击 Verify using email', '3、前两项输入刚刚的邮箱', '4、Send Email，并等我收取邮件', '完成后，截图给我'],
+        ['换区？', '换区教程：https://zh.wikihow.com/在iTunes或App-Store中切换国家'],
+        ['一、开始', '0、所有邮箱都填这个（不要填个人邮箱、苹果id等）', '1、打开验证链接：https://www.myunidays.com/CN/zh-CN', '2、打开教程：http://music.taojingling.cn/am', ':) 遇到问题，截图给我'],
         ['五、未出现pku？', '若未出现PKU列表，需换浏览器打开：https://www.myunidays.com/CN/zh-CN'],
-        ['六、Logout？', '1、点击页面左上角的等于号', '2、点击 Log out'],
-        ['换区？', '换区教程：https://zh.wikihow.com/在iTunes或App-Store中切换国家']
+        ['六、Logout？', '1、点击页面左上角的等于号', '2、点击 Log out']
         // ['丢件？（没有Verify on campus按钮）', '打开链接（较慢）：https://www.myunidays.com/CN/zh-CN/account/register-wayf', '1、点击 Verify on campus', '2、点击 Verify using email', '3、前两项输入刚刚的邮箱', '4、Send Email，并等我收取邮件', '完成后，截图给我'],
       ],
       standbyText: [
         `:) 之后遇到任何问题，随时联系哈。满意的话给小店一个好评哦~`,
-        `补充验证（苹果丢件）：
-1、第一项，填刚刚的邮箱
-2、第二项，填pku，并选择列表第一项PKU
-3、点击 Continue 继续
-
-完成后，截图给我`,
         `补充步骤（苹果邮件延迟或丢失）：
 1、点击 Verify on campus
 2、点击 Verify using email
@@ -126,11 +124,16 @@ export default {
 4、Send Email，并等我收邮件
 
 完成后，截图给我`,
+        `换区教程：https://zh.wikihow.com/在iTunes或App-Store中切换国家`,
+        `0、所有邮箱都填这个（不要填个人邮箱、苹果id等）
+1、打开验证链接：https://www.myunidays.com/CN/zh-CN
+2、打开教程：http://music.taojingling.cn/am
+
+:) 遇到问题，截图给我`,
         `若未出现PKU列表，
 需换浏览器打开：https://www.myunidays.com/CN/zh-CN`,
         `1、点击页面左上角的等于号
-2、点击 Log out`,
-        `换区教程：https://zh.wikihow.com/在iTunes或App-Store中切换国家`
+2、点击 Log out`
         //         `打开链接（较慢）：https://www.myunidays.com/CN/zh-CN/account/register-wayf
         // 1、点击 Verify on campus
         // 2、点击 Verify using email
@@ -142,12 +145,12 @@ export default {
 
       steps: [
         ['https://www.myunidays.com/CN/zh-CN'],
-        ['步骤一', '0、打开验证链接：http://music.taojingling.cn/ap', '1、点击 黑人头像', '2、点击 Get now', '3、点击 Join Now', '完成后，截图给我'],
+        ['步骤一', '0、打开验证链接：https://www.myunidays.com/CN/zh-CN', '1、点击 黑人头像', '2、点击 Get now', '3、点击 Join Now', '完成后，截图给我'],
         ['步骤二', '1、填教育邮箱（一、二项）', '2、您设定密码（三、四项）', '3、性别：Male 男、Female 女', '4、点击 Join Now', '完成后，截图给我'],
         ['步骤三', '1、填姓名（一、二项）', '2、第三项，填pku，并选择列表第一项PKU', '3、左边 Year 1，右边 4 Years', '4、点击 Continue', '完成后，截图给我'],
         ['步骤四', '1、填教育邮箱（一、二项）', '2、点击 Send Email', '完成后，截图给我，并等我收邮件。'],
         ['步骤五', '1、复制长链接到浏览器打开（页面加载很慢，请耐心等待）', '2、点击 Continue（随后自动跳转至Apple Music）',
-          '参考步骤五：http://music.taojingling.cn/ap', ':)遇到问题，随时截图给我哈。']
+          '参考步骤五：http://music.taojingling.cn/am', ':)遇到问题，随时截图给我哈。']
       ],
       stepsText: [
         'https://www.myunidays.com/CN/zh-CN',
@@ -176,7 +179,7 @@ export default {
         `步骤五：1、复制长链接到浏览器打开（页面加载很慢，请耐心等待）
 2、点击 Continue（随后自动跳转至Apple Music）
 
-参考步骤五：http://music.taojingling.cn/ap
+参考步骤五：http://music.taojingling.cn/am
 :)遇到问题，随时截图给我哈。`
       ],
       qr: {},
