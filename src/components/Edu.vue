1f947ab0b8001e4afef32122f429c48e0a854a6e
hdtopku <1450948930@qq.com>
2019-12-11 01:44:52 +0800

    applemusic:前端

---
+++ src/components/Edu.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-tabs v-model="activeName" @tab-click="handleClick">
+    <el-tab-pane label="Apple Music" name="first"><apple-music></apple-music></el-tab-pane>
+    <el-tab-pane label="Jrebel" name="second"><jrebel></jrebel></el-tab-pane>
+    <!-- <el-tab-pane label="Itchat" name="third"><itchat></itchat></el-tab-pane> -->
+  </el-tabs>
+</template>
+<script>
+import AppleMusic from './applemusic/AppleMusic.vue'
+import Jrebel from './jrebel/Jrebel.vue'
+import Itchat from './itchat/Itchat.vue'
+
+export default {
+  components: {
+    AppleMusic,
+    Jrebel,
+    Itchat
+  },
+  data () {
+    return {
+      activeName: 'first'
+    }
+  },
+  methods: {
+    handleClick (tab, event) {
+    }
+  }
+}
+</script>
