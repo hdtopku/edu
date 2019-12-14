c8ff703c42ca5eec76097ce291fb5ac77d30cd9b
hdtopku <1450948930@qq.com>
2019-12-14 19:27:10 +0800

    edu: 优化界面

--- src/components/applemusic/LongLink.vue
+++ src/components/applemusic/LongLink.vue
@@ -85,6 +85,7 @@ export default {
         this.recycle = res.data.recycle
         this.allUse = this.using.concat(this.recycle)
         this.item = res.data.item
+        this.tabs[1].label = '已使用(' + this.used.length + ')'
         if (this.item.id && this.item.id > 0) {
           this.allUse = this.allUse.filter((item, index, arr) => { return item.id !== this.item.id })
           this.allUse = [res.data.item].concat(this.allUse)
