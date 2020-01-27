4514d985da5c72d7d5ed765790003b514e1876cb
hdtopku <1450948930@qq.com>
2020-01-27 17:14:46 +0800

    easyswoole:批量复制长链

--- src/components/jet/Jet.vue
+++ src/components/jet/Jet.vue
@@ -15,7 +15,7 @@
       <h1>404 Not Found</h1>The requested URL was not found. If you entered the URL manually please check your spelling and try again.
     </div>
     <span v-if="res.count > 0"
-      style="position:absolute;top:10px;left:10px;"
+      style="position:absolute;top:10px;left:10px;width:100%;height:100px;color:#DEDEDE;"
       v-long-press="5000"
       @long-press-start="onLongPressStart"
       @long-press-stop="onLongPressStop"
