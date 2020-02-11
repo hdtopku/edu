c024037c332c74124780b7ba959d42489d310834
hdtopku <1450948930@qq.com>
2020-02-11 15:39:10 +0800

    edu:使用cdn

--- src/components/Edu.vue
+++ src/components/Edu.vue
@@ -55,8 +55,8 @@ export default {
     getJrebel: function () {
       const uuid = uuidv1()
       const jrebel =
-        `1、激活邮箱填入：active@jrebel.cn
-2、激活码填入：http://jrebel.taojingling.cn/${uuid}
+        `1、激活码填入：http://jrebel.taojingling.cn/${uuid}
+2、激活邮箱填入：active@jrebel.cn
 3、务必参考宝贝详情第三步，完成激活：https://item.taobao.com/item.htm?id=608435265542`
       setTimeout(() => {
         location.reload()
