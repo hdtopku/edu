22232a8a09ca6ccd564ac5697ebc5294b0881e57
hdtopku <1450948930@qq.com>
2020-02-27 21:09:47 +0800

    edu:加入am教程

--- src/components/applemusic/AM.vue
+++ src/components/applemusic/AM.vue
@@ -1,3 +1,3 @@
 <template>
-<img style="margin-top:3%;width:98%;" src="https://i.loli.net/2020/02/27/gzXQoH9RbWIVPs3.png"/>
+<img style="margin-top:3%;width:98%;" src="http://img.taojingling.cn/1.png"/>
 </template>
