0960deb251a25c6ddc7521c1a16e1e85e9320158
hdtopku <1450948930@qq.com>
2021-03-27 23:04:16 +0800

    自动描述：2021-03-27-23:04:15

--- src/api/mail.js
+++ src/api/mail.js
@@ -8,22 +8,22 @@ export const getQr = () => http.fetchGet('/r?key=qr')
 
 export const toggleItChat = (params = {}) => http.fetchGet('/r?key=itchat', params)
 
-export const getAMs = (params = {}) => http.fetchGet('/sanfsale9831sf', params)
+export const getAMs = (params = {}) => http.fetchGet('/hdanewom', params)
 
-export const syncGetAMs = (params = {}) => http.ajaxFetch('/sanfsale9831sf', params)
+export const syncGetAMs = (params = {}) => http.ajaxFetch('/hdanewom', params)
 
-export const getRData = (params = {}) => http.fetchGet('/sanfsale9831sf/rdata', params)
+export const getRData = (params = {}) => http.fetchGet('/hdanewom/rdata', params)
 
-export const getJet = (params = {}) => http.fetchGet('/sanfsale9831sf/j', params)
+export const getJet = (params = {}) => http.fetchGet('/hdanewom/j', params)
 
-export const syncGetJet = (params = {}) => http.ajaxFetch('/sanfsale9831sf/j', params)
+export const syncGetJet = (params = {}) => http.ajaxFetch('/hdanewom/j', params)
 
-export const getJetCode = (params = {}) => http.fetchPost('/sanfsale9831sf/jc', params)
+export const getJetCode = (params = {}) => http.fetchPost('/hdanewom/jc', params)
 
-export const syncGetJetCode = (params = {}) => http.ajaxFetch('/sanfsale9831sf/jc', params, 'post')
+export const syncGetJetCode = (params = {}) => http.ajaxFetch('/hdanewom/jc', params, 'post')
 
-export const syncGetJetAcount = (params = {}) => http.ajaxFetch('/sanfsale9831sf/ja', params)
+export const syncGetJetAcount = (params = {}) => http.ajaxFetch('/hdanewom/ja', params)
 
-export const batchJetReg = (params = {}) => http.fetchGet('/sanfsale9831sf/jb', params)
+export const batchJetReg = (params = {}) => http.fetchGet('/hdanewom/jb', params)
 
 export const changeSisuMail = (params = {}) => http.fetchGet('/smail', params)
