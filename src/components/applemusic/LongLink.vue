292d0a10f1d0ee4f73f06879149ed07e7931b873
hdtopku <1450948930@qq.com>
2021-03-28 17:13:04 +0800

    自动描述：2021-03-28-17:13:04

--- src/components/applemusic/LongLink.vue
+++ src/components/applemusic/LongLink.vue
@@ -122,6 +122,9 @@ export default {
           }
         })
         this.doCopy(`<div style="color:red;font-size:25px;">${this.items.length}条长链</div>复制成功`, links.join('\r\n'))
+        setTimeout(() => {
+          this.$router.go(0)
+        }, 3500)
       }
       this.operator = res.data.operator
       this.input = ''
@@ -140,9 +143,6 @@ export default {
     clickBatchUse () {
       if (this.select > 0) {
         this.updateAM({ 'operator_id': this.select, 'status': 2, 'count': 10 })
-        setTimeout(() => {
-          this.$router.go(0)
-        }, 4500)
       } else {
         this.openCenter('请选择角色')
       }
