c29ad6e29578fe18c0b792f45b5c4701e676c30c
hdtopku <1450948930@qq.com>
2021-04-03 22:54:00 +0800

    自动描述：2021-04-03-22:54:00

--- src/components/applemusic/AppleGenerate.vue
+++ src/components/applemusic/AppleGenerate.vue
@@ -1,6 +1,30 @@
 <template>
   <div style="text-align: center">
-    <div v-if="used_mails != null && used_mails.hist_mails != null" class="school-container">
+    <div v-if="used_mails != null && used_mails.hist_mails != null">
+    <div class="badge-mails">
+      <el-badge v-if="school !== 'zknu'" :type="school === 'zknu' ? 'danger' : 'info'" :value="used_mails.zknu.length"
+                class="item">
+        <el-button :size="school === 'zknu' ? 'medium' : 'mini'" :type="school === 'zknu' ? 'danger' : ''"
+                   @click="doCopy(used_mails.zknu[0])">
+          {{ school === 'zknu' ? '周口师范学院' : '周' }}
+        </el-button>
+      </el-badge>
+      <el-badge v-if="school !== 'hnucm'" :type="school === 'hnucm' ? 'danger' : 'info'"
+                :value="used_mails.hnucm.length" class="item">
+        <el-button :size="school === 'hnucm' ? 'medium' : 'mini'" :type="school === 'hnucm' ? 'danger' : ''"
+                   @click="doCopy(used_mails.hnucm[0])">
+          {{ school === 'hnucm' ? '湖南中医药大学' : '湖' }}
+        </el-button>
+      </el-badge>
+      <el-badge v-if="school !== 'sisu'" :type="school === 'sisu' ? 'danger' : 'info'" :value="used_mails.sisu.length"
+                class="item">
+        <el-button :size="school === 'sisu' ? 'medium' : 'mini'" :type="school === 'sisu' ? 'danger' : ''"
+                   @click="doCopy(used_mails.sisu[0])">
+          {{ school === 'sisu' ? '四川外国语大学' : '川' }}
+        </el-button>
+      </el-badge>
+    </div>
+    <div class="school-container">
       <el-divider>
         <div v-if="used_mails != null && used_mails.hist_mails != null && used_mails.hist_mails.length > 0">
           <el-popconfirm :loading="clearLoading" cancel-button-text='取消' confirm-button-text='清零' title="是否清零？"
@@ -9,31 +33,17 @@
           </el-popconfirm>
         </div>
       </el-divider>
-      <div class="badge-mails">
-      <el-badge :type="school === 'zknu' ? 'danger' : 'info'" :value="used_mails.zknu.length" class="item">
-        <el-button :size="school === 'zknu' ? 'medium' : 'mini'" :type="school === 'zknu' ? 'danger' : ''"
-                   @click="doCopy(used_mails.zknu[0])">
-          周师范
-        </el-button>
-      </el-badge>
-      <el-badge :type="school === 'hnucm' ? 'danger' : 'info'" :value="used_mails.hnucm.length" class="item">
-        <el-button :size="school === 'hnucm' ? 'medium' : 'mini'" :type="school === 'hnucm' ? 'danger' : ''"
-                   @click="doCopy(used_mails.hnucm[0])">湖中医
-        </el-button>
-      </el-badge>
-      <el-badge :type="school === 'sisu' ? 'danger' : 'info'" :value="used_mails.sisu.length" class="item">
-        <el-button :size="school === 'sisu' ? 'medium' : 'mini'" :type="school === 'sisu' ? 'danger' : ''"
-                   @click="doCopy(used_mails.sisu[0])">川外语
-        </el-button>
-      </el-badge>
-      </div>
     </div>
+    <el-badge  class="selected" :value="used_mails[school].length">
+      <el-button type="danger" @click="doCopy(used_mails[school][0])">{{schoolName}}</el-button>
+    </el-badge>
     <div>
       <el-button :loading="isLoading" class="mail-change" type="primary" @click="throttleChangeMail">
         {{ changeText }}
       </el-button>
     </div>
-<!--        <el-link class="mail-link" type="success" href="https://applemusic-spotlight.myunidays.com/CN/zh-CN?urlset=null" target="_blank">再来一条</el-link>-->
+    </div>
+    <!--        <el-link class="mail-link" type="success" href="https://applemusic-spotlight.myunidays.com/CN/zh-CN?urlset=null" target="_blank">再来一条</el-link>-->
   </div>
 </template>
 
@@ -46,9 +56,9 @@ export default {
     return {
       mails: [],
       school: '',
+      schoolName: '',
       changeText: '更换',
       leftSeconds: 0,
-      radio1: '湖南中医药大学',
       isLoading: false,
       clearLoading: false,
       used_mails: {}
@@ -67,10 +77,13 @@ export default {
         if (histMails != null && histMails.length > 0) {
           if (histMails[0].indexOf('sisu') > 0) {
             this.school = 'sisu'
+            this.schoolName = '四川外国语大学'
           } else if (histMails[0].indexOf('zknu') > 0) {
             this.school = 'zknu'
+            this.schoolName = '周口师范学院'
           } else if (histMails[0].indexOf('hnucm') > 0) {
             this.school = 'hnucm'
+            this.schoolName = '湖南中医药大学'
           }
         }
       }
@@ -92,7 +105,7 @@ export default {
     },
     throttleChangeMail: _.throttle(function () {
       this.changeMail()
-    }, 3000, { 'leading': true, 'trailing': false }),
+    }, 3000, {'leading': true, 'trailing': false}),
     clearMail () {
       this.clearLoading = true
       changeRandomMail({clear: 1})
@@ -137,31 +150,44 @@ export default {
 <style scoped>
 .mail-count {
   font-weight: bolder;
-  font-size: 60px;
+  font-size: 100px;
   cursor: pointer;
   margin: 20px auto;
-  padding: 0 16px;
 }
 
 .school-container {
   height: 80px;
   position: absolute;
   width: 100%;
-  margin-top: 20px;
+  margin-top: 40px;
 }
+
 .badge-mails {
-  margin-top: 50px;
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  width: 96%;
 }
 
 .mail-change {
   font-size: 50px;
   width: 100%;
-  margin-top: 250px;
+  margin-top: 80px;
 }
 
 .item {
   margin: 10px;
 }
+
+.selected {
+  width: 90%;
+  margin-top: 120px;
+}
+.selected button {
+  width: 100%;
+  font-size: 36px;
+}
+
 .mail-link {
   float: left;
   margin-left: 20px;
