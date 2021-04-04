17bafc5ec63ca38542e85d43b477007a0941aedd
hdtopku <1450948930@qq.com>
2021-04-04 22:18:39 +0800

    自动描述：2021-04-04-22:18:39

--- src/components/applemusic/LongLink.vue
+++ src/components/applemusic/LongLink.vue
@@ -211,7 +211,7 @@ export default {
   mounted () {
     this.updateAM()
     this.select = parseInt(getStore('roleSelect') || '1') || 1
-    if (this.select == null || this.select.trim() === '' || this.select < 1) {
+    if (this.select == null || this.select === '' || this.select < 1) {
       this.select = 1
     }
     this.batchCount = getStore('batchCount')
