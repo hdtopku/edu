d3533189e692f9b802a72a975486edfed6dab647
hdtopku <1450948930@qq.com>
2021-10-25 20:12:23 +0800

    自动描述：2021-10-25-20:12:23

--- src/components/Eleme.vue
+++ src/components/Eleme.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-button round style="margin-top: 350px;font-size: 30px;padding: 20px" type="danger" @click="copy">第一步：点我复制口令</el-button>
+    <el-button round style="margin-top: 350px;font-size: 30px;padding: 20px" type="danger" @click="doCopy">第一步：点我复制口令</el-button>
     <div></div>
     <el-button round size="medium" style="margin-top: 50px;font-size: 25px;padding: 20px" type="primary"
                @click="clickButton">第二步：打开饿了么app翻倍
@@ -32,6 +32,10 @@ export default {
       setTimeout(() => {
         window.location.href = 'https://h5.ele.me/ant/qrcode3?open_type=miniapp&url_id=675&inviterId=e8da174e14&actId=1&_ltracker_f=hjb_app_grzx&chInfo=ch_app_chsub_Photo'
       }, 300)
+    },
+    doCopy () {
+      this.copy()
+      this.openCenter(`口令已复制！`)
     },
     copy () {
       this.$copyText('d:/＄XGEXEys＄').catch((err) => {
