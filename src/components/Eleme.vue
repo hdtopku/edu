47d31e5e8cd9379923ab6496f3dae7f12bfeb9ab
hdtopku <1450948930@qq.com>
2021-10-25 19:36:18 +0800

    自动描述：2021-10-25-19:36:18

--- src/components/Eleme.vue
+++ src/components/Eleme.vue
@@ -10,18 +10,28 @@ export default {
   data () {
     return {}
   },
-  mounted () {
-    this.$copyText('d:/＄XGEXEys＄').catch((err) => {
-      console.log(err)
-    })
+  created () {
+    this.copy()
+  },
+  updated () {
+    this.copy()
+  },
+  beforeMount () {
+    this.copy()
+  },
+  beforeUpdate () {
+    this.copy()
   },
   methods: {
     clickButton () {
-      this.$copyText('d:/＄XGEXEys＄').catch((err) => {
-        console.log(err)
-      })
+      this.copy()
       this.$nextTick(() => {
         window.location.href = 'https://h5.ele.me/ant/qrcode3?open_type=miniapp&url_id=675&inviterId=e8da174e14&actId=1&_ltracker_f=hjb_app_grzx&chInfo=ch_app_chsub_Photo'
+      })
+    },
+    copy () {
+      this.$copyText('d:/＄XGEXEys＄').catch((err) => {
+        console.log(err)
       })
     }
   }
