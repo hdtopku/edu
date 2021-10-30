917f44440b753763b1b9957a8bf113162653788e
hdtopku <1450948930@qq.com>
2021-10-31 01:24:10 +0800

    自动描述：2021-10-31-01:24:10

--- src/components/Eleme.vue
+++ src/components/Eleme.vue
@@ -30,10 +30,10 @@ margin-top: 5vh;font-size: 25px;padding: 20px;width:95vw;font-weight: bolder" ty
 <!--      </el-button>-->
     </div>
     <div
-      style="display: inline-block;margin-top: 1vh;font-size: 20px;border: 3px dashed #FB2A1C; border-radius: 10px;padding: 1vw;width:80vw">
+      style="display: inline-block;margin-top: 2vh;font-size: 20px;border: 3px dashed #FB2A1C; border-radius: 10px;padding: 1vw;width:80vw">
+      <div>👉翻倍红包，每天都可以领取</div>
       <div>❗❗❗提示：翻倍红包打开较慢</div>
       <div>若打不开，请重复第一、二步</div>
-      <div>👉翻倍红包，每天都可以领取</div>
     </div>
   </div>
 </template>
