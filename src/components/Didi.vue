2bd09ad9c9f0c6cd16318d2dac36bea4cdc19604
hdtopku <1450948930@qq.com>
2021-11-22 21:00:45 +0800

    自动描述：2021-11-22-21:00:45

--- src/components/Didi.vue
+++ src/components/Didi.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-<img style="width: 100%;" src="https://cdn.nlark.com/yuque/0/2021/jpeg/12454360/1637585919927-aa100d2d-5c16-4dd2-b10b-61338352f8c8.jpeg"/>
+<img style="width: 100%;" src="https://cdn.nlark.com/yuque/0/2021/jpeg/12454360/1637585919927-aa100d2d-5c16-4dd2-b10b-61338352f8c8.jpeg" alt=""/>
 </div>
 </template>
 
